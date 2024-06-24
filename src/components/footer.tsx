fb556d206a034948140b21edb6121350886abd20
nakagawa333 <naka33321@gmail.com>
2024-06-25 01:08:46 +0900

    フッター デザインカンプ反映

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,36 @@
-"use client"
-
-import { useMatchMedia } from "@/hooks/common/useMatchMedia";
-
 export default function Footer(){
-    const isMobileSize:boolean = useMatchMedia("(max-width: 599px)");
-    const headerClass:string = isMobileSize ? "flex flex-wrap bg-gray-400 h-16" : "flex flex-wrap bg-gray-400 h-10";
     
     return(
-        <footer>
-            <div className={headerClass}>
-                {/* <img src="/test.png"></img> */}
-                <p className="text-white ml-5">退職代行比較.com</p>
-                <p className="text-white ml-5">プライバシーポリシー</p>
-                <p className="text-white ml-5">免責事項</p>
-            </div>
-        </footer>
+        <div
+            className={`self-stretch flex flex-col items-start justify-start max-w-full text-left text-sm text-black333333 font-yugothic`}
+        >
+            <footer className="bg-zinc-500 self-stretch bg-gyar-6a6a6a overflow-hidden flex flex-row items-start justify-start py-2.5 px-5 gap-[20px] text-left text-7xs-2 text-black font-inter">
+                <div className="flex flex-col items-start justify-start gap-[8px]">
+                    <div className="flex flex-row items-start justify-start gap-[8px]">
+                        <div className="bg-gainsboro box-border flex flex-row items-start justify-start pt-[5px] pb-1.5 pr-[3px] pl-1 border-[0.4px] border-solid border-black">
+                            <div className="h-[19px] w-[19px] relative bg-gainsboro box-border hidden border-[0.4px] border-solid border-black" />
+                                <div className="relative inline-block min-w-[11px] z-[1]">
+                                    ロゴ
+                                </div>
+                            </div>
+                            <div className="mt-2 text-white relative text-3xs leading-[18.4px] font-medium font-yugothic text-white-fff inline-block min-w-[125px]">
+                                退職代行比較検索.com
+                            </div>
+                        
+                    </div>
+                    <div className="text-white flex flex-row items-start justify-start gap-[40px] text-3xs text-white-fff font-yugothic">
+                        <div className="relative font-medium inline-block min-w-[100px]">
+                            プライバシーポリシー
+                        </div>
+                        <div className="text-white relative font-medium inline-block min-w-[40px]">
+                            免責事項
+                        </div>
+                        <div className="relative font-medium inline-block min-w-[40px]">
+                            お問合せ
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
     )
 }
