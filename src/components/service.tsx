9f8067b4889ffb3d847f0eae8f9a51432a313758
nakagawa333 <naka33321@gmail.com>
2024-07-23 01:45:40 +0900

    退職代行詳細画面 スマホレイアウト実装

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -98,32 +98,74 @@ export default function Page() {
                 <h1 className="text-2xl font-bold mt-0 mb-4">
                     {
                         service && (
-                            <h1 className="text-2xl font-bold mt-0 mb-4">{service.serviceName}</h1>
+                            <h1 className="text-2xl font-bold mt-0 mb-4 border-b-2 mt-6">{service.serviceName}</h1>
                         )
                     }
                 </h1>
             </div>
 
-            <div className="flex flex-wrap">
-                <div className="mx-auto ">
-                    {
-                        service && (
-                            <div className="m-auto w-4/5">
+            <div className="">
+                {
+                    service && (
+                        <div className="">
+                            <div className="flex items-center justify-center">
                                 <img
                                     src={service.imgUrl}
-                                    className="w-4/5"
+                                    className="hover:scale-105 w-11/12"
+                                    style={{
+                                        maxHeight: "180px"
+                                    }}
                                 >
                                 </img>
                             </div>
-                        )
-                    }
 
-                    <div>
-                        <OfficialWebsiteButton
-                            url={serviceData.officialWebsite}
-                        />
-                    </div>
-                </div>
+                            <div className="flex items-center justify-center">
+                                <table className="table-auto w-11/12 mt-8">
+                                    <tbody>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">料金</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.price}円</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">運営元</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.managementName}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">連絡先</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.contactInformationNames}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">無料相談</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.freeConsultation}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">送金保証</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.guaranteeSystem}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">無料プレゼント</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.freeGift}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="border px-4 py-2 bg-gray-200 w-1/4">24時間受付</td>
+                                            <td className="border px-4 py-2 w-3/4">{service.hourService}</td>
+                                        </tr>
+                                    </tbody>
+
+                                </table>
+                            </div>
+
+
+                            <div className="">
+                                <OfficialWebsiteButton
+                                    url={service.officialWebsite}
+                                />
+                            </div>
+                        </div>
+
+                    )
+                }
+
             </div>
 
             {/* <div className="flex flex-wrap">
