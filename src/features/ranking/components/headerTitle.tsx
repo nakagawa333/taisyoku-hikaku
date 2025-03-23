cd5030d1e8a309beae910dd08309686f8d71ef95
nakagawa333 <naka33321@gmail.com>
2025-03-23 23:42:29 +0900

    ランキング一覧 タイトルのレイアウトを修正

--- src/features/ranking/components/headerTitle.tsx
+++ src/features/ranking/components/headerTitle.tsx
@@ -1,16 +1,7 @@
-import Image from 'next/image';
 
 export default function HeaderTitle() {
     return (
         <>
-
-            <Image
-                src="/logo.png"
-                className=""
-                alt=""
-                width={34}
-                height={15}
-            />
             <h1 className="text-2xl font-bold mt-0 mb-4 ml-4">
                 退職代行口コミランキング
             </h1>
--- src/features/ranking/components/ranking.tsx
+++ src/features/ranking/components/ranking.tsx
@@ -5,27 +5,18 @@ import HeaderImage from "./headerImage";
 import HeaderTitle from "./headerTitle";
 import Rankingservices from "./rankingservices";
 
-
 export default function Ranking() {
 
     return (
         <>
 
             <HeaderImage />
-            <div className="mt-6 grid grid-cols-1 sm:grid-cols-3 gap-4">
-                <div className="sm:col-span-2 m-auto overflow-hidden max-w-xs md:max-w-3xl grid grid-cols-1 sm:grid-cols-2 gap-1s">
-                    <div className="flex">
+
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-5">
+                <div className="sm:col-span-2">
+                    <div className="m-auto max-w-xs md:max-w-3xl">
                         <HeaderTitle />
                     </div>
-                    <div>
-
-                    </div>
-                </div>
-                <div></div>
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                <div className="sm:col-span-2">
                     <Rankingservices
                     />
                 </div>
