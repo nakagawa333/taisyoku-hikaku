5b1b359fd9eccf4a9b88d35d99f3750f9bd2a4f4
nakagawa333 <naka33321@gmail.com>
2025-03-24 01:52:07 +0900

    評価グラフ レイアウト修正

--- src/features/service/components/ratingProgress.tsx
+++ src/features/service/components/ratingProgress.tsx
@@ -7,24 +7,21 @@ type Props = {
 export default function RatingProgress(props: Props) {
     const { percentageByRating } = props;
     return (
-        <div className="flex w-full">
+        <div className="flex w-[95%] m-auto">
             <p
-                className="text-center pr-2"
+                className="text-center pr-2 w-[12%] whitespace-nowrap font-bold"
                 style={{
-                    width: "15%"
+                    color: "#FFE87C"
                 }}
             >
                 {'星' + percentageByRating.rating}
             </p>
 
             <div
-                className="bg-gray-200 h-6 mb-6"
-                style={{
-                    width: "80%"
-                }}
+                className="bg-gray-200 h-4 mb-6 mt-1 rounded-[50px] w-[88%]"
             >
                 <div
-                    className="h-full"
+                    className="h-full rounded-[50px]"
                     style={{
                         width: `${percentageByRating.percentage}%`,
                         backgroundColor: "yellow",
