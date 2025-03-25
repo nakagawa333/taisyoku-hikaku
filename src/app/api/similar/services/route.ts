5d445c8e28ec01d1ee0cd3c8de350b491c8aa596
nakagawa333 <naka33321@gmail.com>
2025-03-26 02:08:28 +0900

    類似退職代行取得一覧API タグを取得できるように修正

--- src/app/api/similar/services/route.ts
+++ src/app/api/similar/services/route.ts
@@ -1,4 +1,4 @@
-import { SimilarServicesResponse } from "@/constants/api/response/similarServicesResponse";
+import { SimilarServicesResponse, Tag } from "@/constants/api/response/similarServicesResponse";
 import { fetchServices } from "@/hooks/prisma/services/fetchServices";
 import { fetchSimilarServices } from "@/hooks/prisma/similarServices/fetchSimilarServices";
 import { getStoragePublicUrl } from "@/hooks/supabase/storage/images/getStoragePublicUrl";
@@ -17,87 +17,109 @@ import { NextRequest, NextResponse } from "next/server";
  * 　　　500:
  *         description: 類似退職サービス一覧情報取得失敗時のレスポンス
  */
-export async function GET(request: NextRequest):Promise<NextResponse> {
-    const params:URLSearchParams = request.nextUrl.searchParams;
-    const serviceId:string | null = params.get("serviceId");
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    const params: URLSearchParams = request.nextUrl.searchParams;
+    const serviceId: string | null = params.get("serviceId");
 
     //バリデーションチェック
     let validateError = validate(serviceId);
-    if(validateError) return NextResponse.json({"msg":validateError.details[0].message},{status:400});
+    if (validateError) return NextResponse.json({ "msg": validateError.details[0].message }, { status: 400 });
 
     let similarServices;
 
     let similarServicesQuery = {
-        select:{
-            similar_service_id:true,
+        select: {
+            similar_service_id: true,
         },
         where: {
-            service_id:serviceId
+            service_id: serviceId
         }
     };
 
-    try{
+    try {
         similarServices = await fetchSimilarServices(similarServicesQuery);
-    } catch(error:any){
-        console.error("類似退職代行サービスの取得失敗時のサービスID",serviceId);
+    } catch (error: any) {
+        console.error("類似退職代行サービスの取得失敗時のサービスID", serviceId);
         console.error(error);
         console.error("類似退職代行サービスの取得に失敗しました");
-        return NextResponse.json({"msg":"類似退職代行サービスの取得に失敗しました"},{status:400});
+        return NextResponse.json({ "msg": "類似退職代行サービスの取得に失敗しました" }, { status: 400 });
     }
 
     //取得件数が0件の場合
-    if(!Array.isArray(similarServices) || 0 === similarServices.length){
+    if (!Array.isArray(similarServices) || 0 === similarServices.length) {
         return NextResponse.json(
             {
-                similarServices:[]
+                similarServices: []
             }
         );
     }
 
     let services;
     let servicesQuery = {
-        select:{
-            service_id:true,
-            service_name:true,
-            image_file_path:true,
-            image_bucketss:true,
+        select: {
+            service_id: true,
+            service_name: true,
+            image_file_path: true,
+            image_bucketss: true,
+            service_tags: {
+                select: {
+                    tags: {
+                        select: {
+                            tag_id: true,
+                            tag_name: true
+                        }
+                    }
+                }
+            }
         },
     };
 
-    try{
+    try {
         services = await fetchServices(servicesQuery);
-    } catch(error:any){
+    } catch (error: any) {
         console.error(error);
         console.error("退職代行サービスの取得に失敗しました");
-        return NextResponse.json({"msg":"退職代行サービスの取得に失敗しました"},{status:400});
+        return NextResponse.json({ "msg": "退職代行サービスの取得に失敗しました" }, { status: 400 });
     }
 
-    const servicesMap:Map<string,any> = new Map();
-    for(let service of services){
-        servicesMap.set(service.service_id,service);
+    const servicesMap: Map<string, any> = new Map();
+    for (let service of services) {
+        servicesMap.set(service.service_id, service);
     }
 
-    const similarServicesResponses:SimilarServicesResponse[] = [];
+    const similarServicesResponses: SimilarServicesResponse[] = [];
 
-    for(let similarService of similarServices){
-        const similarServiceId:string = similarService.similar_service_id;
-        const service:any | undefined = servicesMap.get(similarServiceId);
+    for (let similarService of similarServices) {
+        const similarServiceId: string = similarService.similar_service_id;
+        const service: any | undefined = servicesMap.get(similarServiceId);
 
-        if(service !== undefined){
-            let publicUrl:string = "";
+        if (service !== undefined) {
+            let publicUrl: string = "";
 
-            try{
-                const { data } = getStoragePublicUrl('images',service.image_file_path);
+            try {
+                const { data } = getStoragePublicUrl('images', service.image_file_path);
                 publicUrl = data.publicUrl;
-            } catch(error:any){
+            } catch (error: any) {
                 console.error(error);
                 console.error(`サービスID名${service.service_id}の画像取得に失敗しました`);
             }
 
-            const similarServicesResponse:SimilarServicesResponse = {
-                similarServiceId:similarService.similar_service_id,
-                similarServiceName:service.service_name,
-                imgUrl:publicUrl
+            let tags: Tag[] = [];
+            const serviceTags = service.service_tags;
+            if (Array.isArray(serviceTags)) {
+                tags = serviceTags.map((serviceTag) => {
+                    return {
+                        tagId: serviceTag?.tags?.tag_id,
+                        tagName: serviceTag?.tags?.tag_name
+                    }
+                })
+            }
+
+            const similarServicesResponse: SimilarServicesResponse = {
+                similarServiceId: similarService.similar_service_id,
+                similarServiceName: service.service_name,
+                imgUrl: publicUrl,
+                tags: tags
             }
             similarServicesResponses.push(similarServicesResponse);
         }
@@ -105,7 +127,7 @@ export async function GET(request: NextRequest):Promise<NextResponse> {
 
     return NextResponse.json(
         {
-            similarServices:similarServicesResponses
+            similarServices: similarServicesResponses
         }
     );
 }
