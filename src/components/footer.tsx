427510f1629de387edf68dfb081e9c1f338caaf3
nakagawa333 <naka33321@gmail.com>
2025-04-09 00:29:24 +0900

    フッターロゴ削除

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,16 +11,9 @@ export default function Footer() {
                 className="bg-zinc-500 self-stretch bg-gyar-6a6a6a overflow-hidden flex flex-row items-start justify-start py-2.5 px-5 gap-[20px] text-left text-7xs-2 text-black font-inter">
                 <div className="mx-auto flex flex-col items-start justify-start gap-[8px]">
                     <div className="flex flex-row items-start justify-start gap-[8px]">
-                        <div className="bg-gainsboro box-border flex flex-row items-start justify-start pt-[5px] pb-1.5 pr-[3px] pl-1 border-[0.4px] border-solid border-black">
-                            <div className="h-[19px] w-[19px] relative bg-gainsboro box-border hidden border-[0.4px] border-solid border-black" />
-                            <div className="relative inline-block min-w-[11px] z-[1]">
-                                ロゴ
-                            </div>
-                        </div>
                         <div className="mt-2 text-white relative text-3xs leading-[18.4px] font-medium font-yugothic text-white-fff inline-block min-w-[125px]">
                             🄫退職代行口コミランキング ALL rights reserved.
                         </div>
-
                     </div>
                     <div className="mx-auto text-white flex flex-row items-start justify-start gap-[40px] text-3xs text-white-fff font-yugothic">
                         <div className="relative font-medium inline-block min-w-[100px]">
