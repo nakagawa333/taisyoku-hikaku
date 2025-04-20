a490e71ef404a8c8952644d15cd26eadb31e1894
nakagawa333 <naka33321@gmail.com>
2025-04-21 01:54:38 +0900

    Supabaseの認証処理実装

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,62 +1,22 @@
 "use client"
-import { Paths } from "@/constants/common/paths";
-import Link from "next/link";
-import { FormEvent, useRef } from "react";
+import supabase from "@/libs/supabase/supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import * as ja from "../../localization/ja/ja.json";
 
+//TODO ログイン後の認証処理(アクセストークンをlocalStorageに保存するなど)の実装
 //ログイン画面
 export const Login = () => {
-    const email = useRef<HTMLInputElement>(null);
-    const password = useRef<HTMLInputElement>(null);
-
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-    }
-
     return (
-        <div className="flex justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">ログイン</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            メールアドレス
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            ref={email}
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            パスワード
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            ref={password}
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <Link
-                            href={Paths.SIGNUP}
-                            className="py-2 text-blue-500"
-                        >
-                            アカウントを作成
-                        </Link>
-                        <button
-                            type="submit"
-                            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        >
-                            ログイン
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div >
+        <div className="w-96 mx-auto">
+            <Auth
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                providers={["google"]}
+                localization={{
+                    variables: ja
+                }}
+            />
+        </div>
     );
 }
