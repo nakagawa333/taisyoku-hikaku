009fa9541e929e474b24e018ea0b16b8ec39767c
nakagawa333 <naka33321@gmail.com>
2025-04-27 00:32:13 +0900

    CORS対応

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const nextConfig = {
     reactStrictMode: false,
+    // 全ての API routes にマッチ
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_URL },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig;
